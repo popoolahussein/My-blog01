4a695af5f386167caf5e7e09de1e62076ce76820
popoolahussein <popoolakehindehussein@gmail.com>
2024-10-27 00:25:49 +0100

    docs: edited <p> text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Posts from "./components/Posts"
 export default function Home() {
   return (
       <main className="px-6 mx-auto">
-        <p className="mt-12 mb-12 text-3xl text-center dark:text-white"><i>codeMadeEasy◩&nbsp;</i> By
+        <p className="mt-12 mb-12 text-3xl text-center dark:text-white"><i>expensiveCodes◩&nbsp;</i> By
           <span className="whitespace-nowrap"> <span className="font-bold italic">Hussein</span></span>
         </p>
         <Posts />
